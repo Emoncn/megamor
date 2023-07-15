2f20565c325b4b0be5e3140cb762eab7864a1fa9
Tancrède SUARD <gitlab.commits@tancou.me>
2023-07-15 15:59:03 +0200

    :musical_note: Salut Yelle

--- Megamor/Assets/MovementController.cs
+++ Megamor/Assets/MovementController.cs
@@ -2,48 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BiteScriptée : MonoBehaviour
+public class MovementController : MonoBehaviour
 {
-
     public float
         x = 0,
         y = 0,
         z = 0,
-        jumpState = 0;
+        speed = 1;
 
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Transform transform = this.GetComponent<Transform>();
-        //this.x = this.x + 0.01f;
-        //transform.position = new Vector3(this.x, this.y, this.z);
-
         float
             moveX = 0,
             moveY = this.y,
             moveZ = 0;
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            moveX = 0.1f;
+            moveX = 0.1f * this.speed;
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            moveX = -0.1f;
+            moveX = -0.1f * this.speed;
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            moveZ = 0.1f;
+            moveZ = 0.1f * this.speed;
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            moveZ = -0.1f;
+            moveZ = -0.1f * this.speed;
         }
-        transform.position = new Vector3(this.x += moveX, moveY, this.z += moveZ);
 
+        this.GetComponent<Transform>().position = new Vector3(
+            this.x += moveX,
+            moveY,
+            this.z += moveZ
+        );
     }
 }
