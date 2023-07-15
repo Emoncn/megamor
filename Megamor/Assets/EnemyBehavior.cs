42143014290b1d594b897ff3cf11c75df29bf175
Tancrède SUARD <gitlab.commits@tancou.me>
2023-07-16 01:49:52 +0200

    :fire: HE IS ALIVE

--- Megamor/Assets/EnemyBehavior.cs
+++ Megamor/Assets/EnemyBehavior.cs
@@ -17,9 +17,8 @@ public class EnemyBehavior : MonoBehaviour
     {
         // Rotate the camera every frame so it keeps looking at the target
         transform.LookAt(target);
+        Vector3 targetpos = new Vector3(target.transform.position.x, transform.position.y, target.position.z);
 
-		Vector3 Targetposition = new Vector3 (target.position.x, transform.position.y, target.position.z);
-
-        this.transform.position = Vector3.MoveTowards(transform.position, Targetposition, speed);
+        this.transform.position = Vector3.MoveTowards(transform.position, targetpos, speed);
     }
 }
